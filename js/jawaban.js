792fed052153bcbc7fa1dca205102d0f40335a12
listrikstatis <mediaskripsi1@gmail.com>
2021-07-23 13:35:19 +0800

    first commit

--- js/jawaban.js
+++ js/jawaban.js
@@ -34,17 +34,17 @@ function readlah() {
 
     tmp.innerHTML = "";
     if (kelasnya.value == "1") {
-        kelasfix = "IX A";
+        kelasfix = "XII A";
     } else if (kelasnya.value == "2") {
-        kelasfix = "IX B";
+        kelasfix = "XII B";
     } else if (kelasnya.value == "3") {
-        kelasfix = "IX C";
+        kelasfix = "XII C";
     } else if (kelasnya.value == "4") {
-        kelasfix = "IX D";
+        kelasfix = "XII D";
     } else if (kelasnya.value == "5") {
-        kelasfix = "IX E";
+        kelasfix = "XII E";
     } else if (kelasnya.value == "6") {
-        kelasfix = "IX F";
+        kelasfix = "XII F";
     } 
 
     // if (sekolah.value == "1") {
